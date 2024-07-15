32786469d5cafa2d11f92e9acdb4629f23781d9e
Amee Assad <aa3628@columbia.edu>
2024-07-15 14:43:43 +0200

    change to np.int32_t in cpu_nms for makefile

--- pose_estimation/code/hrnet/lib/nms/cpu_nms.pyx
+++ pose_estimation/code/hrnet/lib/nms/cpu_nms.pyx
@@ -28,7 +28,7 @@ def cpu_nms(np.ndarray[np.float32_t, ndim=2] dets, np.float thresh):
     cdef np.ndarray[np.int32_t, ndim=1] order = scores.argsort()[::-1].astype('i')
 
     cdef int ndets = dets.shape[0]
-    cdef np.ndarray[np.in32_t, ndim=1] suppressed = \
+    cdef np.ndarray[np.int32_t, ndim=1] suppressed = \
             np.zeros((ndets), dtype=np.int)
 
     # nominal indices
